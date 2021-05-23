cc113c9f4d9a89f2200de2ee07411bc365d9d503
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-05-23 17:11:23 +0200

    Ajout du bug pour le screencast

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -5,7 +5,7 @@ const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
     .then(res => res.json())
     .catch(err => console.log("Oh no", err))
 
